667db230dcb87e2f69c5e90a3d73dd7050491589
Shaojun-J <sjmyreg@outlook.com>
2023-11-06 00:13:15 -0500

    Finished ReservationWin CarSelection, update Signup, add inven
    1. add inventory
    2. add staff
    3. add Car
    4. update Customer
    5. update Admin
    6. add DBA
    
    Co-Authored-By: Jennifer <100975749+Jennifer328@users.noreply.github.com>

---
+++ CarRental/Models/Staff.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Models
+{
+    internal class Staff
+    {
+        public int id { get; set; } = 0;
+        public string user_name { get; set; } = "";
+        public string password { get; set; } = "";
+        public string salt { get; set; } = "";
+        public string phone { get; set; } = "";
+        public string email { get; set; } = "";
+        public int authority { get; set; } = 0;
+
+        //public static implicit operator Staff(Object obj)
+        //{
+        //    Staff staff = obj as Staff;
+        //    return staff;
+        //}
+    }
+}
--- CarRental/Signup.xaml.cs
+++ CarRental/Signup.xaml.cs
@@ -1,8 +1,10 @@
-﻿using Npgsql;
+﻿using CarRental.Models;
+using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
@@ -17,6 +19,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using static Npgsql.Replication.PgOutput.Messages.RelationMessage;
+using CarRental.Models;
 
 namespace CarRental
 {
@@ -25,39 +28,13 @@ namespace CarRental
     /// </summary>
     public partial class Signup : Window
     {
+        
         public Signup()
         {
             InitializeComponent();
         }
 
-        private string get_ConnectionString()
-        {
-            string host = "host=localhost;";
-            string port = "port=5432;";
-            string dbName = "database=vanierdb;";
-            string userName = "username=postgres;";
-            string password = "password=328111;";
-            //string connectionString = string.Format("{0}{1}{2}{3}{4}", host, port, dbName, userName, password);
-            string connectionString = $"{host} {port} {dbName} {userName} {password}";
-            return connectionString;
-        }
-
-        private NpgsqlConnection establishConnection()
-        {
-            NpgsqlConnection con = null;
-            try
-            {
-                con = new NpgsqlConnection(get_ConnectionString());
-                //MessageBox.Show("Connection established.");
-            }
-            catch (NpgsqlException ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            return con;
-        }
-
-        
+               
         private bool checkInfor()
         {
             Regex regex = null;
@@ -120,104 +97,97 @@ namespace CarRental
             }
 
 
-            if (tb_licenseId.Text.Length <= 0)
-            {
-                lb_licenseid.Foreground = new SolidColorBrush(Colors.Red);
-                bErr = true;
-            }
-            else
-            {
-                lb_licenseid.Foreground = new SolidColorBrush(Colors.Black);
-            }
+            //if (tb_licenseId.Text.Length <= 0)
+            //{
+            //    lb_licenseid.Foreground = new SolidColorBrush(Colors.Red);
+            //    bErr = true;
+            //}
+            //else
+            //{
+            //    lb_licenseid.Foreground = new SolidColorBrush(Colors.Black);
+            //}
 
-            //regex = new Regex(@"^(\d{4})-(\d{2})-(\d{2})$");
-            //Match m = regex.Match(tb_issuedate.Text);
-            //Console.WriteLine(m.Groups[1].Value);
-            //Console.WriteLine(m.Groups[2].Value);
+            //bool bResult = DateTime.TryParse(tb_issuedate.Text, out DateTime issueDateTime);
+            //if (!bResult)
+            //{
+            //    lb_issuedate.Foreground = new SolidColorBrush(Colors.Red);
+            //    bErr = true;
+            //}
+            //else
+            //{
+            //    lb_issuedate.Foreground = new SolidColorBrush(Colors.Black);
+            //}
 
-            bool bResult = DateTime.TryParse(tb_issuedate.Text, out DateTime issueDateTime);
-            if (!bResult)
-            {
-                lb_issuedate.Foreground = new SolidColorBrush(Colors.Red);
-                bErr = true;
-            }
-            else
-            {
-                lb_issuedate.Foreground = new SolidColorBrush(Colors.Black);
-            }
+            //bResult = DateTime.TryParse(tb_expiredate.Text, out DateTime expireDateTime);
+            //if (!bResult)
+            //{
+            //    lb_expiredate.Foreground = new SolidColorBrush(Colors.Red);
+            //    bErr = true;
+            //}
+            //else
+            //{
+            //    lb_expiredate.Foreground = new SolidColorBrush(Colors.Black);
+            //}
 
-            //regex = new Regex(@"^(\d){4}-(\d){2}-((\d){2})$");
-            bResult = DateTime.TryParse(tb_expiredate.Text, out DateTime expireDateTime);
-            if (!bResult)
-            {
-                lb_expiredate.Foreground = new SolidColorBrush(Colors.Red);
-                bErr = true;
-            }
-            else
-            {
-                lb_expiredate.Foreground = new SolidColorBrush(Colors.Black);
-            }
+            //if(expireDateTime <= issueDateTime)
+            //{
+            //    lb_expiredate.Foreground = new SolidColorBrush(Colors.Red);
+            //    lb_issuedate.Foreground = new SolidColorBrush(Colors.Red);
+            //    bErr = true;
+            //}
 
-            if(expireDateTime <= issueDateTime)
-            {
-                lb_expiredate.Foreground = new SolidColorBrush(Colors.Red);
-                lb_issuedate.Foreground = new SolidColorBrush(Colors.Red);
-                bErr = true;
-                //MessageBox.Show("date err");
-            }
-
-            if (tb_type.Text.Length <= 0)
-            {
-                lb_type.Foreground = new SolidColorBrush(Colors.Red);
-                bErr = true;
-            }
-            else
-            {
-                lb_type.Foreground = new SolidColorBrush(Colors.Black);
-            }
+            //if (tb_type.Text.Length <= 0)
+            //{
+            //    lb_type.Foreground = new SolidColorBrush(Colors.Red);
+            //    bErr = true;
+            //}
+            //else
+            //{
+            //    lb_type.Foreground = new SolidColorBrush(Colors.Black);
+            //}
 
 
-            if (tb_street.Text.Length <= 0)
-            {
-                lb_street.Foreground = new SolidColorBrush(Colors.Red);
-                bErr = true;
-            }
-            else
-            {
-                lb_street.Foreground = new SolidColorBrush(Colors.Black);
-            }
-            if (tb_city.Text.Length <= 0)
-            {
-                lb_city.Foreground = new SolidColorBrush(Colors.Red);
-                bErr = true;
-            }
-            else
-            {
-                lb_city.Foreground = new SolidColorBrush(Colors.Black);
-            }
-            if (tb_province.Text.Length <= 0)
-            {
-                lb_province.Foreground = new SolidColorBrush(Colors.Red);
-                bErr = true;
-            }
-            else
-            {
-                lb_province.Foreground = new SolidColorBrush(Colors.Black);
-            }
-            if (tb_postalcode.Text.Length <= 0)
-            {
-                lb_postalcode.Foreground = new SolidColorBrush(Colors.Red);
-                bErr = true;
-            }
-            else
-            {
-                lb_postalcode.Foreground = new SolidColorBrush(Colors.Black);
-            }
+            //if (tb_street.Text.Length <= 0)
+            //{
+            //    lb_street.Foreground = new SolidColorBrush(Colors.Red);
+            //    bErr = true;
+            //}
+            //else
+            //{
+            //    lb_street.Foreground = new SolidColorBrush(Colors.Black);
+            //}
+            //if (tb_city.Text.Length <= 0)
+            //{
+            //    lb_city.Foreground = new SolidColorBrush(Colors.Red);
+            //    bErr = true;
+            //}
+            //else
+            //{
+            //    lb_city.Foreground = new SolidColorBrush(Colors.Black);
+            //}
+            //if (tb_province.Text.Length <= 0)
+            //{
+            //    lb_province.Foreground = new SolidColorBrush(Colors.Red);
+            //    bErr = true;
+            //}
+            //else
+            //{
+            //    lb_province.Foreground = new SolidColorBrush(Colors.Black);
+            //}
+            //if (tb_postalcode.Text.Length <= 0)
+            //{
+            //    lb_postalcode.Foreground = new SolidColorBrush(Colors.Red);
+            //    bErr = true;
+            //}
+            //else
+            //{
+            //    lb_postalcode.Foreground = new SolidColorBrush(Colors.Black);
+            //}
 
             return bErr;
 
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
             NpgsqlConnection con = null;
 
@@ -248,42 +218,44 @@ namespace CarRental
                 string email = tb_email.Text; //tb_email.Text; //"jgodber1@1und1.de"
 
 
-                var licenseObj = new
+                //var licenseObj = new
+                //{
+                //    license_id = tb_licenseId.Text,
+                //    issue_date = tb_issuedate.Text,
+                //    expire_date = tb_expiredate.Text,
+                //    type = tb_type.Text,
+                //};
+                //var license = JsonSerializer.Serialize(licenseObj);
+                //Console.WriteLine(license);
+                //var addressObj = new
+                //{
+                //    street = tb_street.Text,
+                //    city = tb_city.Text,
+                //    tb_province = tb_province.Text,
+                //    Postal_code = tb_postalcode.Text,
+                //};
+                //var address = JsonSerializer.Serialize(addressObj);
+
+                CustomerAuthority customer = new CustomerAuthority();
+                customer.user_name = tb_userName.Text;
+                customer.password= tb_password.Password; // MyUtility.getMD5(tb_password.Password + MyUtility.SALT); // tb_password.Password;
+                customer.salt = MyUtility.getMD5(DateTime.Now.ToString());
+                customer.email = tb_email.Text;
+                customer.phone = tb_phone.Text;
+
+                var serverResponse = await DBA.client.PostAsJsonAsync("AddCustomer", customer); 
+                
+                if (serverResponse.IsSuccessStatusCode)
                 {
-                    license_id = tb_licenseId.Text,
-                    issue_date = tb_issuedate.Text,
-                    expire_date = tb_expiredate.Text,
-                    type = tb_type.Text,
-                };
-
-
-                var license = JsonSerializer.Serialize(licenseObj);
-                Console.WriteLine(license);
-                var addressObj = new
+                    MessageBox.Show("Register Successful");
+                    MainWindow newWin = new MainWindow();
+                    newWin.Show();
+                    this.Close();
+                }
+                else
                 {
-                    street = tb_street.Text,
-                    city = tb_city.Text,
-                    tb_province = tb_province.Text,
-                    Postal_code = tb_postalcode.Text,
-                };
-                var address = JsonSerializer.Serialize(addressObj);
-
-                string userName = tb_userName.Text;
-                string password = MyUtility.getMD5(tb_password.Password + MyUtility.SALT); // tb_password.Password;
-                string salt = MyUtility.getMD5(DateTime.Now.ToString());
-
-                con = establishConnection();
-                con.Open();
-                string Query = $"insert into car_rental.customer (first_name, last_name, phone, email, driver_license, address, username, password, salt) " +
-                    $"values('{firstName}', '{lastName}', '{phone}', '{email}', '{license}', '{address}', '{userName}', '{password}', '{salt}')";
-                NpgsqlCommand cmd = new NpgsqlCommand(Query, con);
-
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Register successfully");
-
-                MainWindow newWin = new MainWindow();
-                newWin.Show();
-                this.Close();
+                    MessageBox.Show("Register failed");
+                }
             }
             catch (Exception ex)
             {
